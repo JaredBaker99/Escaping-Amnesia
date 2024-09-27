8a98b08494dbe1526cf88af849e6fc29fab7194b
JaredBaker99 <j.k.baker99@outlook.com>
2024-09-26 21:56:41 -0400

    Add scene random switching

---
+++ Escaping Amnesia/Assets/Scripts/RandomSceneSwaper.cs
@@ -0,0 +1,28 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneSwapper : MonoBehaviour
+{
+    
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        // Debug message before trigger check
+        UnityEngine.Debug.Log("OnTriggerEnter2D started.");
+
+        if (collision.CompareTag("Player"))
+        {
+            string[] scenes = { "RectangleBattle-1", "RectangleBattle-2", "MainMenu", "SpawnRoom", "BossRoom-1" };
+            int randomIndex = Random.Range(0, scenes.Length - 1); // Corrected range
+            UnityEngine.Debug.Log("Scenes array length: " + scenes.Length);
+            UnityEngine.Debug.Log("Random index: " + randomIndex);
+            UnityEngine.Debug.Log("Selected scene: " + scenes[randomIndex]);
+
+            string randomScene = scenes[randomIndex];
+
+            SceneManager.LoadScene(randomScene);
+        }
+
+        // Debug message after trigger check
+        UnityEngine.Debug.Log("OnTriggerEnter2D finished.");
+    }
+}
