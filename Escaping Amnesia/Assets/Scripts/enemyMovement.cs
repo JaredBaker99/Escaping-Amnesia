f8389582102629ac2b9501cb96f4c1dfea4ebc54
Garrett6419 <garrettbequette6419@gmail.com>
2024-09-29 20:29:27 -0400

    Enemy AI updates
    Fixed issue where enemy paused in front of player by fixing up the player prefab. Now has a tracking distance to detect the player and won't stop tracking the player.

--- Escaping Amnesia/Assets/Scripts/enemyMovement.cs
+++ Escaping Amnesia/Assets/Scripts/enemyMovement.cs
@@ -7,18 +7,25 @@ public class enemyMovement : MonoBehaviour
 {
     public GameObject player;
     public float speed ;
+    public float tracking ;
 
     private float distance ;
+    private bool found ;
    
 
     void Update()
-{
-    distance = Vector2.Distance(transform.position, player.transform.position);
-    Vector2 direction = player.transform.position - transform.position;
+        {
+            distance = Vector2.Distance(transform.position, player.transform.position);
+            Vector2 direction = player.transform.position - transform.position;
 
-    transform.position = Vector2.MoveTowards(this.transform.position, player.transform.position, speed * Time.deltaTime);
+            if(found || distance < tracking) {
+                if(!found) {
+                    found = true ;
+                }
+                transform.position = Vector2.MoveTowards(this.transform.position, player.transform.position, speed * Time.deltaTime);
+            }
+            
 
-
-}
+        }
 
 }
