42a627198741e039ca22bae47871a6261308955f
JaredBaker99 <j.k.baker99@outlook.com>
2024-10-06 22:15:53 -0400

    Create rooms
    Create a total of 9 rooms. and added come objects and player character

--- Escaping Amnesia/Assets/Scripts/RandomSceneSwaper.cs
+++ Escaping Amnesia/Assets/Scripts/RandomSceneSwaper.cs
@@ -11,7 +11,7 @@ public class SceneSwapper : MonoBehaviour
 
         if (collision.CompareTag("Player"))
         {
-            string[] scenes = { "RectangleBattle-1", "RectangleBattle-2", "RectangleBattle-3", "RectangleBattle-4", "RectangleBattle-5" };
+            string[] scenes = { "RectangleBattle-1", "RectangleBattle-2", "RectangleBattle-3", "RectangleBattle-4", "RectangleBattle-5", "ElbowBattle-1", "ElbowBattle-2", "CircleBattle-1", "CircleBattle-2" };
             int randomIndex = Random.Range(0, scenes.Length - 1); // Corrected range
             UnityEngine.Debug.Log("Scenes array length: " + scenes.Length);
             UnityEngine.Debug.Log("Random index: " + randomIndex);
