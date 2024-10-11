9c904eec4d02c5f8af96ab00b544cdcf871b7803
Garrett6419 <garrettbequette6419@gmail.com>
2024-10-11 15:31:11 -0400

    Added Character Animation
    Added assets for the character/enemies and animated the player movement. Also changed the playerController Script to use the animations

--- Escaping Amnesia/Assets/Scripts/playerConroller.cs
+++ Escaping Amnesia/Assets/Scripts/playerConroller.cs
@@ -6,6 +6,7 @@ public class PlayerController : MonoBehaviour
 {
     public float moveSpeed = 5f;  // Speed of the player movement
     public Rigidbody2D rb;        // Reference to the Rigidbody2D component
+    public Animator animator ;
 
     Vector2 movement;  // Store the player's movement input
 
@@ -15,6 +16,27 @@ public class PlayerController : MonoBehaviour
         // Get input from player for horizontal (left/right) and vertical (up/down) movement
         movement.x = Input.GetAxisRaw("Horizontal");  // A/D or Left/Right Arrow
         movement.y = Input.GetAxisRaw("Vertical");    // W/S or Up/Down Arrow
+        
+        if(movement.x == 0 && movement.y == 0) {
+            animator.SetBool("Moving", false);
+            
+        }
+        else {
+            animator.SetBool("Moving", true);
+            if(movement.x > 0) {
+                animator.Play("WalkingRight") ;
+            }
+            else if(movement.x < 0){
+                animator.Play("WalkingLeft") ;
+            }
+            else if(movement.y > 0) {
+                animator.Play("WalkingUp") ;
+            }
+            else if(movement.y < 0) {
+                animator.Play("WalkingDown") ;
+            }
+        }
+        
     }
 
     // FixedUpdate is called at a fixed interval and is used for physics calculations
