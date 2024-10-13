cdd813bd979077c3eaea3f5d9daf5f7105540c0f
Frederick B <89982743+bouloutef04@users.noreply.github.com>
2024-10-13 15:31:30 -0400

    Look at description.
    Added dialogue to the Spawn Room. This includes a dialogue canvas and a dialogue script that prints the characters once at a time, a fast froward using a click, and pauses the game while the dialog is up.
    I also added the PauseMenu (and it's other aspects) along with the OverworldAudio to the scene.
    Additionally, I placed the game objects within the spawnroom scene into on empty object for organization and added more music files.

---
+++ Escaping Amnesia/Assets/Scripts/UI_Scripts/Dialogue.cs
@@ -0,0 +1,59 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class Dialogue : MonoBehaviour
+{
+    public TextMeshProUGUI textComponent;
+    public string[] lines;
+    public float textSpeed;
+    private int index;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Time.timeScale = 0f;
+        textComponent.text = string.Empty;
+        StartDialogue();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetMouseButtonDown(0)){//Skip lines
+            if(textComponent.text == lines[index]){//Proceeds to next line
+                NextLine();
+            }
+            else{//Skips to the end of the line
+                StopAllCoroutines();
+                textComponent.text = lines[index];
+            }
+        }
+    }
+
+    void StartDialogue(){
+        index = 0;
+        StartCoroutine(TypeLine());
+    }
+
+    IEnumerator TypeLine(){
+        foreach (char c in lines[index].ToCharArray()){//Prints char by char
+            textComponent.text += c;
+            yield return new WaitForSecondsRealtime(textSpeed);
+        }
+    }
+
+    void NextLine(){//Goes to next line
+        if (index < lines.Length - 1){
+            index++;
+            textComponent.text = string.Empty;
+            StartCoroutine(TypeLine());
+        }
+        else{//Once lines are finished, set Dialogue to false and resume
+            gameObject.SetActive(false);
+            Time.timeScale = 1f;
+        }
+    }
+}
