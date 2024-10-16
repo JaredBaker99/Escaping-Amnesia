4369f4ba4764a89fed73db4e3fcb2ad09488a3f0
Frederick B <89982743+bouloutef04@users.noreply.github.com>
2024-10-15 21:00:17 -0400

    Look at description.
    Added some SFX files.
    Got rid of the options button in the main menu.
    Adjusted the audio sound in both the main menu and the Overworld.
    Added a fade out when quitting to main menu.
    Created a new UI Document that holds the old UI in the case that I go back to fix it.

--- Escaping Amnesia/Assets/Scripts/AudioScripts/DoNotDestroy.cs
+++ Escaping Amnesia/Assets/Scripts/AudioScripts/DoNotDestroy.cs
@@ -1,15 +1,37 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.SceneManagement;
 public class DoNotDestroy : MonoBehaviour
 {
-    private void Awake(){
+    private void Awake()
+    {
         GameObject[] musicObj = GameObject.FindGameObjectsWithTag("OverworldAudio");
 
-        if(musicObj.Length > 1){
+        // //When loading back into the MainMenu scene, destroy objectd
+        // Scene scene = SceneManager.GetActiveScene();
+        // Debug.Log("" + scene.name);
+        // if (scene.name == "MainMenu")
+        // {
+        //     Destroy(gameObject);
+        //     return;
+        // }
+
+        //If more than one object exists
+        if (musicObj.Length > 1)
+        {
             Destroy(this.gameObject);
         }
-        DontDestroyOnLoad(this.gameObject); 
+        DontDestroyOnLoad(this.gameObject);
+
+
+
+
+    }
+
+    public void ChangedScenes()//Used to destroy objects when needed (mainmenu)
+    {
+        Destroy(this.gameObject);
     }
 }
