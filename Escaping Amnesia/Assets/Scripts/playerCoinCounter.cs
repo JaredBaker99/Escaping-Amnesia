999aca91224047b13b09bb6cbdee99147ba3698a
JaredBaker99 <j.k.baker99@outlook.com>
2024-11-04 17:47:25 -0500

    working on coin ui

--- Escaping Amnesia/Assets/Scripts/playerCoinCounter.cs
+++ Escaping Amnesia/Assets/Scripts/playerCoinCounter.cs
@@ -9,10 +9,4 @@ using TMPro;
 public class playerCoinCounter : MonoBehaviour
 {
     public int currentCoinCount;
-    [SerializeField] private TMP_Text coinText;
-
-    //void Update()
-    //{
-    //    coinText.text = currentCoinCount.ToString();
-    //}
 }
