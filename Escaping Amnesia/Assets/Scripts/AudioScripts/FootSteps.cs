03db31f7bf32aec494ed8a4c78b57e355407fbfc
Frederick B <89982743+bouloutef04@users.noreply.github.com>
2024-11-15 13:50:04 -0500

    Overworld Audio & Dialogue
    Added soundFX for text during dialogue. Added foodstep script but no sound is present.
    
    Changed overworld audio script to have more audio for layering and transitioning between different tracks.

---
+++ Escaping Amnesia/Assets/Scripts/AudioScripts/FootSteps.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FootSteps : MonoBehaviour
+{
+    public OverworldAudioManager overworldAudio;
+    public AudioClip footSteps;
+
+    void Update(){
+        if (
+            Input.GetKey(KeyCode.UpArrow) || Input.GetKey(KeyCode.DownArrow) ||
+            Input.GetKey(KeyCode.LeftArrow) || Input.GetKey(KeyCode.RightArrow) ||
+            Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.W) ||
+            Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.D)
+            ){
+            overworldAudio.PlaySFX(footSteps);
+        }
+    }
+    
+}
