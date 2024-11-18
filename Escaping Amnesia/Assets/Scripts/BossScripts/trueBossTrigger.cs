1b6ec1380cabfa2ca304e2872a12c3cd6758a7a4
Garrett6419 <garrettbequette6419@gmail.com>
2024-11-18 12:19:34 -0500

    Bug Fixes
    Fixed bug where the trigger area of the final boss didn't turn off

--- Escaping Amnesia/Assets/Scripts/BossScripts/trueBossTrigger.cs
+++ Escaping Amnesia/Assets/Scripts/BossScripts/trueBossTrigger.cs
@@ -9,7 +9,7 @@ public class trueBossTrigger : MonoBehaviour
 
     private void OnTriggerEnter2D(Collider2D collision) {
         // Check if the collision is with the player
-        if (collision.gameObject.CompareTag("Player")) {
+        if (collision.gameObject.CompareTag("Player") && boss.GetComponent<trueBossMovement>().isAlive) {
             player.GetComponent<PlayerController>().turnOffMovement() ;
             boss.GetComponent<trueBossMovement>().playAttack() ;
         }
